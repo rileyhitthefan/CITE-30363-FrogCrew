b70b2a2629f45fb298165c27a49ea722f8a0cbd0
riley <riley.phan@tcu.edu>
2025-03-23 23:29:39 -0500

    First commit

---
+++ src/main/java/edu/tcu/cs/frogcrewbackend/FrogCrewBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.frogcrewbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogCrewBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogCrewBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/frogcrewbackend/FrogCrewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.frogcrewbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogCrewBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
