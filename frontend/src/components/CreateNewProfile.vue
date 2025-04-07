9d8ba2c21b35f9d777c36bc8f0f0be24f21ca030
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-04-07 17:51:03 +0200

    form

---
+++ frontend/src/components/CreateNewProfile.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <form action="#">
+            First Name: <input
+                type="text"
+                required
+            />
+            <br />
+            Last Name: <input
+                type="text"
+                required
+            />
+            <br />
+            Email: <input
+                type="email"
+                required
+            />
+            <br />
+            Phone Number: <input
+                type="tel"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9{4}]"
+                required
+            />
+            <br />
+            Password: <input
+                type="password"
+            />
+            <br />
+            Role: <input
+                type="text"
+                required
+            />
+            <br />
+            Qualified Position: <input
+                type="text"
+                required
+            />
+            <br />
+
+        </form>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
