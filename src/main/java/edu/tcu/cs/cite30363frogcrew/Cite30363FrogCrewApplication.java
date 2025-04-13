998501ebe9e7828aa02d76886f2dcfd5ed8e6263
riley <riley.phan@tcu.edu>
2025-04-13 17:13:59 -0500

    update gitignore

---
+++ src/main/java/edu/tcu/cs/cite30363frogcrew/Cite30363FrogCrewApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.cite30363frogcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cite30363FrogCrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cite30363FrogCrewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/cite30363frogcrew/Cite30363FrogCrewApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.cite30363frogcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cite30363FrogCrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
