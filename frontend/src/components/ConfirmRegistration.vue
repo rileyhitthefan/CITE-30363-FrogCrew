fa284770d7c04e863c34a8f3be2fdcc5984c9f2b
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-04-14 17:39:20 +0200

    Confirm Registration

---
+++ frontend/src/components/ConfirmRegistration.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button>Confirm Registration</button>
+        <button>Modify details</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
