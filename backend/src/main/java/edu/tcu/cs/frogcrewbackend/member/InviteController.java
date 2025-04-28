7ed4892562951d8f822cc3b117b467877f8bd457
riley <riley.phan@tcu.edu>
2025-04-28 12:59:21 -0500

    #7 - validate invite token

--- backend/src/main/java/edu/tcu/cs/frogcrewbackend/member/InviteController.java
+++ backend/src/main/java/edu/tcu/cs/frogcrewbackend/member/InviteController.java
@@ -4,10 +4,10 @@ import edu.tcu.cs.frogcrewbackend.member.dto.InviteDTO;
 import edu.tcu.cs.frogcrewbackend.system.Result;
 import edu.tcu.cs.frogcrewbackend.system.StatusCode;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("${api.endpoint.base-url}/invite")
@@ -22,5 +22,18 @@ public class InviteController {
     public Result inviteMember(@RequestBody @Valid InviteDTO invite) {
         this.inviteService.sendInvite(invite.emails());
         return new Result(true, StatusCode.SUCCESS, "Invite success", invite);
+    }
+
+    @GetMapping("/{token}")
+    public Result validateInviteToken(@PathVariable String token) {
+        boolean isValid = this.inviteService.validateInviteToken(token);
+
+        if (isValid) {
+            Map<String, String> tokenData = new HashMap<>();
+            tokenData.put("token", token);
+            return new Result(true, StatusCode.SUCCESS, "Invitation valid", tokenData);
+        } else {
+            return new Result(false, StatusCode.NOT_FOUND, "Invitation not valid", null);
+        }
     }
 }
