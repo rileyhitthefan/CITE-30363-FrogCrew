a829f5bae233122829d554cfc7ea2cd4d266d224
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-04-30 04:20:40 +0200

    Use case #9 complited! (missing confirmation that shift has been picked up, will implement with use case that picks up shifts)

--- frontend/src/apis/notifications.js
+++ frontend/src/apis/notifications.js
@@ -5,7 +5,7 @@ const BASE_URL = 'http://localhost:3000/notification'
  * Not defined on API documentation but needed for multiple use cases
  * Use Case 9
  */
-const submitNotification = async ({ userId, message, read, date }) => {   
+const submitNotification = async ({ userId, userIdCreatedNotification,  message, read, date }) => {   
     try {
         const response = await fetch(BASE_URL, {
             method: 'POST',
@@ -14,6 +14,7 @@ const submitNotification = async ({ userId, message, read, date }) => {
             },
             body: JSON.stringify({
                 userId,
+                userIdCreatedNotification,
                 message,
                 read,
                 date
@@ -31,4 +32,62 @@ const submitNotification = async ({ userId, message, read, date }) => {
     }
 }
 
-export default { submitNotification }
+
+/**
+ * GET /notifications/{userId}
+ * Return a list of notifications for a specific user
+ * Use Case 9
+ */
+const getNotificationsById = async (userId) => {
+    try {
+        const response = await fetch(`${BASE_URL}?userId=${userId}`)
+        
+        if (!response.ok) {
+            throw new Error(`Error fetching crew member: ${response.statusText}`)
+        }
+        const data = await response.json(); 
+        return data;
+
+    } catch (error) {
+        console.error(error)
+        throw error //Rethrow the error to be caught by the caller
+    }
+}
+
+
+/**
+ * PUT /notifications/{notificationId}
+ * Mark a notification as read
+ * Use Case 9
+ */
+const markNotificationAsRead = async (id, userId, userIdCreatedNotification, message, read, date) => {
+    try {
+        const response = await fetch(`${BASE_URL}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userId,
+                userIdCreatedNotification,
+                message,
+                read,
+                date
+            }),
+        });
+        
+        if (!response.ok) {
+            throw new Error('Failed to update member');
+        }
+
+        const data = await response.json()
+        return data
+
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
+}
+
+
+export default { submitNotification, getNotificationsById, markNotificationAsRead }
